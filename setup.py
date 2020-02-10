87f30139f65bba2edd2aab26ee075e6aca45a7df
Fabio Piras <f.piras@dunbar.it>
2020-02-10 09:51:33 +0100

    More info

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="GDPR helpers",
+    version="v0.1-alpha",
+    description="A Django app for managing GDPR compliancy",
+    url="https://github.com/Arussil/django-gdpr-helpers",
+    author="Piras Fabio",
+    author_email="fabio@godsofweb.com",
+    packages=["gdpr_helpers"],
+    install_requires=["django>2.2,<3.0"],
+    license = "BSD-3-Clause",
+)
