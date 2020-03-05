8f3ee3e2947c8dfa5d7c10fd3752329f7f8f7473
Fabio Piras <f.piras@dunbar.it>
2020-03-05 17:24:03 +0100

    Fixed so there's no exceptio without a LegalReasonGroup

--- gdpr_helpers/forms.py
+++ gdpr_helpers/forms.py
@@ -4,9 +4,12 @@ from .models import LegalReasonGroup, PrivacyLog
 class GDPRFormMixin(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        group = LegalReasonGroup.objects.get(where=self.Meta.where)
-        for reason in group.get_as_form_fields():
-            self.fields[reason["field_name"]] = reason["field"]
+        try:
+            group = LegalReasonGroup.objects.get(where=self.Meta.where)
+            for reason in group.get_as_form_fields():
+                self.fields[reason["field_name"]] = reason["field"]
+        except LegalReasonGroup.DoesNotExist:
+            pass
 
     def save(self, *args, **kwargs):
         saved_object = super().save(*args, **kwargs)
