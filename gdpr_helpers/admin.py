f6b1af9364e8ba501142ceadade94ae0aa16e025
Fabio Piras <f.piras@dunbar.it>
2020-03-05 16:37:07 +0100

    Added admin for new models

--- gdpr_helpers/admin.py
+++ gdpr_helpers/admin.py
@@ -1,19 +1,44 @@
 from django.contrib import admin
 
-from .models import LegalReason, PrivacyEvent, PrivacyLog
+from .models import LegalReason, LegalReasonGroup, PrivacyEvent, PrivacyLog
 
 
-class PrivacyEventInline(admin.StackedInline):
+class NoPermissionMixin(object):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class PrivacyEventInline(NoPermissionMixin, admin.StackedInline):
     extra = 0
     model = PrivacyEvent
 
 
-class PrivacyLogAdmin(admin.ModelAdmin):
-    fields = ("user", "created")
-    readonly_fields = ("created",)
+class PrivacyLogAdmin(NoPermissionMixin, admin.ModelAdmin):
+    fields = ("get_generic_fk", "created")
+    readonly_fields = ("get_generic_fk", "created")
     inlines = [PrivacyEventInline]
+
+    def get_generic_fk(self, obj):
+        return obj.content_object
+
+    get_generic_fk.short_description = "Related object"
+
+
+class LegalReasonInline(admin.StackedInline):
+    extra = 1
+    model = LegalReason
+
+
+class LegalReasonGroupAdmin(admin.ModelAdmin):
+    inlines = [LegalReasonInline]
 
 
 admin.site.register(LegalReason)
-admin.site.register(PrivacyEvent)
+admin.site.register(LegalReasonGroup, LegalReasonGroupAdmin)
 admin.site.register(PrivacyLog, PrivacyLogAdmin)
