02c294466ef6fe7de1fe81c3431bfa62311fc0f6
Fabio Piras <f.piras@dunbar.it>
2020-05-29 12:15:31 +0200

    Some fixes to structure

---
+++ gdpr_helpers/__init__.py
@@ -0,0 +1,4 @@
+VERSION = (0, 1, 3)
+__version__ = ".".join(str(i) for i in VERSION)
+
+default_app_config = "gdpr_helpers.apps.GdprHelpersConfig"
--- gdpr_helpers/apps.py
+++ gdpr_helpers/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class GdprHelpersConfig(AppConfig):
     name = "gdpr_helpers"
+    verbose_name = _("Gdpr helpers")
